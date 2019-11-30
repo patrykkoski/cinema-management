ef72af902e8e4bbf06c013d887d2d760cc86ec5e
Patryk <patrykkoski1994@gmail.com>
2019-11-30 20:26:06 +0100

    Initial commit

---
+++ src/main/java/pl/patrykkoski/Cinema/Management/CinemaManagementApplication.java
@@ -0,0 +1,13 @@
+package pl.patrykkoski.Cinema.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaManagementApplication.class, args);
+	}
+
+}
